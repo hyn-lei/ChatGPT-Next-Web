f96a6fefef582e02a00b5737d435bf4831828be2
hyn.x.lei@gmail.com <ne2AJPqfi8NE8FK>
2023-07-20 08:36:39 +0800

    1. upstream error fixed.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./styles/markdown.scss";
 import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "ChatGPT Next Web",
@@ -34,6 +35,20 @@ export default function RootLayout({
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-HN22XZPC9P"></Script>
+        <Script id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HN22XZPC9P');
+        `}
+        </Script>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5820497200009964"
+          crossOrigin="anonymous"
+        ></Script>
       </head>
       <body>{children}</body>
     </html>
